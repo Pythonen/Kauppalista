2131b177d64b1ec0ab7cd702690edf55bb89351b
Aleksi Puttonen <aleksi.puttonen@gmail.com>
2020-08-30 19:41:11 +0300

    Toimii koneella

---
+++ kauppalista_socket_api/app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask_socketio import SocketIO, emit
+
+groceries = []
+print(groceries)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode='gevent')
+
+@app.route('/')
+def index():
+    return ('<h1>This is the index page of the server that is serving KauppaLista</h1>')
+
+@socketio.on('connect')
+def handle_connection():
+    emit('FromAPI', groceries)
+
+@socketio.on('adding-grocery')
+def handle_message(message):
+    print('moi')
+    print('received message: ' + message)
+    groceries.append(message)
+    print(groceries)
+    emit('AfterAdding', groceries, broadcast=True)
+
+@socketio.on('deleting-grocery')
+def handle_deleting(message):
+    print('to be deleted: ' + message)
+    groceries.remove(message)
+    print(groceries)
+    emit('AfterDeleting', groceries, broadcast=True)
+
+@socketio.on_error_default  # handles all namespaces without an explicit error handler
+def default_error_handler(e):
+    print(e)
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
